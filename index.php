8bde7ae2e920d42b6b7f52d688b9b666b54757b3
Arturo Santana <arturo.santtana@gmail.com>
2024-04-13 15:48:47 +0000

    📘 docs: first commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="apple-touch-icon" sizes="180x180" href="img/apple-touch-icon.png">
+      <link rel="icon" type="image/png" sizes="32x32" href="img/favicon-32x32.png">
+      <link rel="icon" type="image/png" sizes="16x16" href="img/favicon-16x16.png">
+      <link rel="manifest" href="img/site.webmanifest">
+      <title>Inicio Session</title>
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+   </head>
+   <body>
+      <link href="css/sign-in.css" rel="stylesheet">
+      </head>
+      <body class="d-flex align-items-center py-4 bg-body-tertiary">
+         <main class="form-signin w-100 m-auto">
+            <form>
+                <div style="display: flex; justify-content: center;">
+                    <img class="mb-3" src="img/logo.png" alt="CPG LOGO" width="100" height="100">
+                </div>
+               <h1 class="h3 mb-3 fw-normal">Inicia Session</h1>
+               
+               <input class="form-control mb-2" type="text" placeholder="Usuario" aria-label="Introduce Usuario">
+               <input class="form-control mb-2" type="password" placeholder="Contraseña" aria-label="Introduce Contraseña">
+
+               <div class="form-check text-start my-3">
+                  <input class="form-check-input" type="checkbox" value="remember-me" id="flexCheckDefault">
+                  <label class="form-check-label" for="flexCheckDefault"> Recuerdame </label>
+               </div>
+               <button class="btn btn-primary w-100 py-2" type="button">Iniciar Session</button>
+            </form>
+         </main>
+         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+   </body>
+</html>
