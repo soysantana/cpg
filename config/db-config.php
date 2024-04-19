8c96e09a9da67e8b7fefa009615c113709d8b9f4
Arturo Santana <arturo.santtana@gmail.com>
2024-04-19 21:50:44 +0000

    ⚙ add: db connection configuration

---
+++ config/db-config.php
@@ -0,0 +1,8 @@
+<?php
+
+  define( 'DB_HOST', 'localhost' );          // Set database host
+  define( 'DB_USER', 'root' );             // Set database user
+  define( 'DB_PASS', '@Manager9223' );      // Set database password
+  define( 'DB_NAME', 'cpg' );             // Set database name
+
+?>
