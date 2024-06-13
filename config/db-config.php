a114c618e8503bbc62515e88c5e62cb0aa88be6f
Santana <arturo.santtana@gmail.com>
2024-06-13 11:21:52 -0400

    🐛 fix: cryptic errors fixed

--- config/db-config.php
+++ config/db-config.php
@@ -3,6 +3,6 @@
   define( 'DB_HOST', 'localhost' );          // Set database host
   define( 'DB_USER', 'root' );             // Set database user
   define( 'DB_PASS', '' );      // Set database password
-  define( 'DB_NAME', 'cpg' );             // Set database name
+  define( 'DB_NAME', 'loan_system' );             // Set database name
 
 ?>
